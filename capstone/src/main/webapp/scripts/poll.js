18d93655e0cf876401c1dcf63ae2a85d14748c49
Lucy Qu <lucyqu@Lucys-MacBook-Pro.local>
2020-06-22 19:14:11 -0400

    add checkbox

--- capstone/src/main/webapp/scripts/poll.js
+++ capstone/src/main/webapp/scripts/poll.js
@@ -1,9 +1,21 @@
 function createOptionElement(text) {
-  console.log(text);
   const optionsContainer = document.getElementById("options-container");
-  const optionElement = document.createElement("div");
-  optionElement.innerText = text;
-  optionsContainer.appendChild(optionElement);
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  //replace with some id
+  checkbox.id = "id";
+  //replace with suggestion name
+  checkbox.name = "name";
+  //replace with value
+  checkbox.value = "value";
+  const label = document.createElement("label");
+  //replace with id
+  label.htmlFor = "id";
+  label.appendChild(document.createTextNode(text));
+  const br = document.createElement("br");
+  optionsContainer.appendChild(checkbox);
+  optionsContainer.appendChild(label);
+  optionsContainer.appendChild(br);
 }
 
 /**
