14e8177b4b92ed68a3670e156213e34ecd666385
anikabagga <anikabagga@gmail.com>
2020-06-22 17:51:53 +0000

    add object files

---
+++ capstone/src/main/java/com/google/sps/Objects/Group.java
@@ -0,0 +1,23 @@
+ package com.google.sps.data;
+
+
+public final class Group {
+
+    Array
+
+
+    public Group ()
+
+
+}
+ 
+ 
+ 
+ 
+ 
+  Challenge[] challenges,
+  User[] members,
+  Post[] posts,
+  Poll poll,
+  String groupName,
+  Image header
---
+++ capstone/src/main/java/com/google/sps/servlets/GroupServlet.java
@@ -0,0 +1,17 @@
+ 
+ 
+ package com.google.sps.data;
+
+
+public final class 
+ 
+ 
+ 
+ 
+ 
+  Challenge[] challenges,
+  User[] members,
+  Post[] posts,
+  Poll poll,
+  String groupName,
+  Image header
