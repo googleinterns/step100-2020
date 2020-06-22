7d34e9607b9564f8205f666c5b5ffce23d07426c
Lucy Qu <lucyqu@Lucys-MacBook-Pro.local>
2020-06-22 14:17:56 -0400

    fix package import

--- capstone/src/main/java/com/google/sps/Objects/Option.java
+++ capstone/src/main/java/com/google/sps/Objects/Option.java
@@ -1,24 +1,26 @@
-package com.google.sps.data;
+package com.google.sps.Objects;
+
+import java.util.ArrayList;
 
 public final class Option {
 
-    private final String text;
-    private ArrayList<UserId> votes;
+  private final String text;
+  private ArrayList<String> votes;
 
-    public Option(String text){
-        this.text = text;
-        this.votes = new ArrayList<UserId>();
-    }
+  public Option(String text) {
+    this.text = text;
+    this.votes = new ArrayList<String>();
+  }
 
-    public String getText(){
-        return this.text;
-    }
+  public String getText() {
+    return this.text;
+  }
 
-    public void addVote(UserId id){
-        this.votes.add(id);
-    }
+  public void addVote(String id) {
+    this.votes.add(id);
+  }
 
-    public ArrayList<UserId> getVotes() {
-        return votes;
-    }
+  public ArrayList<String> getVotes() {
+    return votes;
+  }
 }
--- capstone/src/main/java/com/google/sps/Objects/User.java
+++ capstone/src/main/java/com/google/sps/Objects/User.java
@@ -1,47 +1,47 @@
-package com.google.sps.data;
+package com.google.sps.Objects;
 
+import java.util.ArrayList;
 
-public final class User{
+public final class User {
 
-    private final int userId;
-    private final String name;
-    private final String email;
-    private final ArrayList<Badge> badges;
-    private final ArrayList<Group> groups;
-    private final ArrayList<String> interests;
- 
-    public User(int userId, String name, String email, ArrayList<Badge> badges, ArrayList<Group> groups, ArrayList<String> interests) {
-        this.userId = userId;
-        this.name = name;
-        this.email = email;
-        this.badges = badges;
-        this.groups = groups;
-        this.interests = interests;
-    }
+  private final int userId;
+  private final String name;
+  private final String email;
+  private final ArrayList<Badge> badges;
+  private final ArrayList<Group> groups;
+  private final ArrayList<String> interests;
 
-    public int getUserId() {
-        return userId;
-    }
+  public User(int userId, String name, String email, ArrayList<Badge> badges,
+      ArrayList<Group> groups, ArrayList<String> interests) {
+    this.userId = userId;
+    this.name = name;
+    this.email = email;
+    this.badges = badges;
+    this.groups = groups;
+    this.interests = interests;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public int getUserId() {
+    return userId;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public ArrayList<Badge> getBadges() {
-        return badges;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public ArrayList<Group> getGroups() {
-        return groups;
-    }
-    
-    public ArrayList<String> getInterests() {
-        return interests;
-    }
+  public ArrayList<Badge> getBadges() {
+    return badges;
+  }
+
+  public ArrayList<Group> getGroups() {
+    return groups;
+  }
+
+  public ArrayList<String> getInterests() {
+    return interests;
+  }
 }
-
-
