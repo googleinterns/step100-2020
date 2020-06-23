d8e998ba5bc457c20c2cbf6d8c9adbac40078698
Lucy Qu <lucy_qu@brown.edu>
2020-06-23 13:33:05 -0400

    add margin space

--- capstone/src/main/webapp/scripts/poll.js
+++ capstone/src/main/webapp/scripts/poll.js
@@ -1,21 +1,22 @@
 function createOptionElement(text) {
-  const optionsContainer = document.getElementById("options-container");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  //replace with some id
-  checkbox.id = "id";
-  //replace with suggestion name
-  checkbox.name = "name";
-  //replace with value
-  checkbox.value = "value";
-  const label = document.createElement("label");
-  //replace with id
-  label.htmlFor = "id";
-  label.appendChild(document.createTextNode(text));
-  const br = document.createElement("br");
-  optionsContainer.appendChild(checkbox);
-  optionsContainer.appendChild(label);
-  optionsContainer.appendChild(br);
+  // const optionsContainer = document.getElementById("options-container");
+  // const checkbox = document.createElement("input");
+  // checkbox.type = "checkbox";
+  // //replace with some id
+  // checkbox.id = "id";
+  // //replace with suggestion name
+  // checkbox.name = "name";
+  // //replace with value
+  // checkbox.value = "value";
+  // const label = document.createElement("label");
+  // //replace with id
+  // label.htmlFor = "id";
+  // label.appendChild(document.createTextNode(text));
+  // const br = document.createElement("br");
+  // optionsContainer.appendChild(checkbox);
+  // optionsContainer.appendChild(label);
+  // optionsContainer.appendChild(br);
+  console.log(text);
 }
 
 /**
