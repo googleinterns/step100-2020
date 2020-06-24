088b5aca72b5cd192ddf5182412d89b3d2a90892
Lucy Qu <lucy_qu@brown.edu>
2020-06-24 11:00:00 -0400

    edit Option class and add comments

--- capstone/src/main/java/com/google/sps/Objects/Option.java
+++ capstone/src/main/java/com/google/sps/Objects/Option.java
@@ -2,24 +2,52 @@ package com.google.sps.Objects;
 
 import java.util.ArrayList;
 
+/**
+ * Represents each option of the poll. Has text field and keeps track of users
+ * who voted for each option.
+ * 
+ * @author lucyqu
+ *
+ */
 public final class Option {
 
   private final String text;
+  // List of userIds of people who voted for option
   private ArrayList<String> votes;
 
+  /**
+   * Constructor that takes in text and initializes list of user votes.
+   *
+   * @param text String representing option name
+   */
   public Option(String text) {
     this.text = text;
     this.votes = new ArrayList<String>();
   }
 
+  /**
+   * Returns text for each option.
+   *
+   * @return String text
+   */
   public String getText() {
     return this.text;
   }
 
-  public void addVote(String id) {
-    this.votes.add(id);
+  /**
+   * Add userId to list to keep track of users who voted for option.
+   *
+   * @param userId user id
+   */
+  public void addVote(String userId) {
+    this.votes.add(userId);
   }
 
+  /**
+   * Get users who voted for option.
+   *
+   * @return list of user ids
+   */
   public ArrayList<String> getVotes() {
     return votes;
   }
