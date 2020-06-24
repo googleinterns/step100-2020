6ff016bd7fad857657f9260c16759e5dd1c9b512
Lucy Qu <lucy_qu@brown.edu>
2020-06-24 13:47:54 -0400

    factor out poll format into HTML template

--- capstone/src/main/java/com/google/sps/servlets/PollServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/PollServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.sps.Objects.Option;
 
+@WebServlet("/poll")
 public class PollServlet extends HttpServlet {
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     Option option1 = new Option("Option 1");
--- capstone/src/main/webapp/scripts/poll.js
+++ capstone/src/main/webapp/scripts/poll.js
@@ -6,67 +6,33 @@ function getPollOptions() {
     .then(response => response.json())
     .then(options => {
       options.forEach(option => {
-        createOptionElementLoad(option);
+        // createOptionElementLoad(option);
+        renderOptionElement(option);
       });
     });
 }
 
-function createOptionElementLoad(option) {
+function renderOptionElement(option) {
   let innerBar = "200";
-  let numVotes = option["userVotes"].length;
+  let numVotes = option["votes"].length;
   let text = option["text"];
 
   const optionsContainer = document.getElementById("options-container");
-  const optionContainer = document.createElement("option-container");
-  optionContainer.setAttribute("class", "option-container");
-  //Add challenge name
-  const challengeName = document.createElement("p");
+  const optionElement = document.getElementById("option-template");
+  const optionElementNode = document.importNode(optionElement.content, true);
+  //Set name of challenge
+  const challengeName = optionElementNode.querySelector("p");
   const challengeText = document.createTextNode(text);
   challengeName.appendChild(challengeText);
-  optionContainer.appendChild(challengeName);
-  //Poll bar
-  const svgElement = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "svg"
-  );
-  svgElement.setAttribute("width", BAR_WIDTH);
-  svgElement.setAttribute("height", BAR_HEIGHT);
-  const outerRect = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "rect"
-  );
-  outerRect.setAttribute("width", BAR_WIDTH);
-  outerRect.setAttribute("height", BAR_HEIGHT);
-  outerRect.setAttribute("class", "poll-rect");
-  const innerRect = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "rect"
-  );
-  innerRect.setAttribute("width", innerBar);
-  innerRect.setAttribute("height", BAR_HEIGHT);
-  innerRect.setAttribute("class", "poll-inner-rect");
-  svgElement.appendChild(outerRect);
-  svgElement.appendChild(innerRect);
-  optionContainer.appendChild(svgElement);
-  //Create checkbox
-  const checkContainer = document.createElement("span");
-  checkContainer.setAttribute("class", "check-container");
-  const inputElement = document.createElement("input");
-  inputElement.setAttribute("type", "checkbox");
-  inputElement.setAttribute("id", text);
-  const labelElement = document.createElement("label");
-  labelElement.htmlFor = text;
-  checkContainer.appendChild(inputElement);
-  checkContainer.appendChild(labelElement);
-  optionContainer.appendChild(checkContainer);
-  //Create votes label
-  const votesLabel = document.createElement("div");
-  votesLabel.setAttribute("class", "votes-label");
+  const checkbox = optionElementNode.querySelector("input");
+  checkbox.id = text;
+  const label = optionElementNode.querySelector("label");
+  label.htmlFor = text;
+  //Set number of votes per challenge option
+  const votesLabel = optionElementNode.getElementById("num-votes");
   const votesString = numVotes === 1 ? "task" : "tasks";
   votesLabel.innerText = `${numVotes} ${votesString}`;
-  optionContainer.appendChild(votesLabel);
-  //Append each option to outer options container
-  optionsContainer.appendChild(optionContainer);
+  optionsContainer.appendChild(optionElementNode);
 }
 
 function createOptionElement(text) {
