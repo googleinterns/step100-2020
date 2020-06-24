e2ed529bc76d480ba7a283394dc62a017148503c
Lucy Qu <lucy_qu@brown.edu>
2020-06-24 12:52:48 -0400

    poll functionality

--- capstone/src/main/java/com/google/sps/Objects/Option.java
+++ capstone/src/main/java/com/google/sps/Objects/Option.java
@@ -2,25 +2,53 @@ package com.google.sps.Objects;
 
 import java.util.ArrayList;
 
+/**
+ * Represents each option of the poll. Has text field and keeps track of users
+ * who voted for each option.
+ *
+ * @author lucyqu
+ *
+ */
 public final class Option {
 
   private final String text;
-  private ArrayList<Integer> votes;
+  // List of userIds of people who voted for option
+  private ArrayList<String> votes;
 
+  /**
+   * Constructor that takes in text and initializes list of user votes.
+   *
+   * @param text String representing option name
+   */
   public Option(String text) {
     this.text = text;
-    this.votes = new ArrayList<Integer>();
+    this.votes = new ArrayList<String>();
   }
 
+  /**
+   * Returns text for each option.
+   *
+   * @return String text
+   */
   public String getText() {
     return this.text;
   }
 
-  public void addVote(int id) {
-    this.votes.add(id);
+  /**
+   * Add userId to list to keep track of users who voted for option.
+   *
+   * @param userId user id
+   */
+  public void addVote(String userId) {
+    this.votes.add(userId);
   }
 
-  public ArrayList<Integer> getVotes() {
+  /**
+   * Get users who voted for option.
+   *
+   * @return list of user ids
+   */
+  public ArrayList<String> getVotes() {
     return votes;
   }
 }
