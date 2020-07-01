b6622a292f0245c2dd16b3529184def9b6ee283d
anikabagga <anikabagga@gmail.com>
2020-07-01 22:01:40 +0000

    change groups to contain member ids

--- capstone/src/main/java/com/google/sps/Objects/Group.java
+++ capstone/src/main/java/com/google/sps/Objects/Group.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 
 public final class Group {
 
-  private final ArrayList<User> members;
+  private final ArrayList<String> memberIds;
   private final ArrayList<Challenge> challenges;
   private final ArrayList<Post> posts;
   private final Poll poll;
   private final String groupName;
   private final String headerImg;
 
-  public Group(ArrayList<User> members, ArrayList<Challenge> challenges, ArrayList<Post> posts, 
+  public Group(ArrayList<String> membersIds, ArrayList<Challenge> challenges, ArrayList<Post> posts, 
       Poll poll, String groupName, String headerImg) {
-    this.members = members;
+    this.memberIds = memberIds;
     this.challenges = challenges;
     this.posts = posts;
     this.poll = poll;
@@ -21,8 +21,8 @@ public final class Group {
     this.headerImg = headerImg;
   }
 
-  public ArrayList<User> getMembers() {
-    return members;
+  public ArrayList<String> getMemberIds() {
+    return memberIds;
   }
 
   public ArrayList<Challenge> challenges() {
@@ -49,8 +49,8 @@ public final class Group {
     this.challenges.add(newChallenge);
   }
 
-  public void addMember(User newMember) {
-    this.members.add(newMember);
+  public void addMember(String memberId) {
+    this.memberIds.add(memberId);
   }
 
   public void addPost(Post newPost) {
---
+++ capstone/src/main/java/com/google/sps/servlets/GroupMembersServlet.java
@@ -0,0 +1,25 @@
+package com.google.sps.servlets;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import error.ErrorHandler;
+
+@WebServlet("/group-members")
+
+public class UpdateLikesServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
