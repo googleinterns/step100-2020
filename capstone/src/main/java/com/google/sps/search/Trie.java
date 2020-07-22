c2d569c9e78daf1dbdddbdcc54109073997f8d12
Lucy Qu <lucy_qu@brown.edu>
2020-07-22 14:42:55 -0400

    commenting

--- capstone/src/main/java/com/google/sps/search/Trie.java
+++ capstone/src/main/java/com/google/sps/search/Trie.java
@@ -7,17 +7,33 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
+/**
+ * Class representing the Trie data structure. Contains children to indicate child nodes in the data
+ * structure and whether current node is the end of the name.
+ *
+ * @author lucyqu
+ */
 public class Trie implements Serializable {
 
   private static final long serialVersionUID = 1L;
   private Map<String, Trie> children;
   private boolean isEnd;
 
+  /**
+   * Constructor that sets the children of trie node and whether current node marks the end of the
+   * name.
+   */
   public Trie() {
     this.children = new HashMap<String, Trie>();
     this.isEnd = false;
   }
 
+  /**
+   * Inserts name into trie, with full name being the last node appending to the end of the name.
+   *
+   * @param name user first name or last name
+   * @param fullName user full name.
+   */
   public void insert(String name, String fullName) {
     if (name.contentEquals("")) {
       this.isEnd = true;
@@ -31,6 +47,13 @@ public class Trie implements Serializable {
     }
   }
 
+  /**
+   * Returns set of strings with names that match the given prefix.
+   *
+   * @param prefix prefix to search in trie
+   * @param totalPrefix parameter needed to build string
+   * @return set of strings with matching prefix
+   */
   public Set<String> searchWithPrefix(String prefix, String totalPrefix) {
     if (prefix.equals("")) {
       return findAll(new TreeSet<String>(), totalPrefix);
@@ -44,6 +67,13 @@ public class Trie implements Serializable {
     }
   }
 
+  /**
+   * Helper method used to find all words from a certain node.
+   *
+   * @param names set of matching names
+   * @param prefix characters from the trie built up to current node
+   * @return set of full names formed from node in Trie
+   */
   private Set<String> findAll(Set<String> names, String prefix) {
     Map<String, Trie> possibilities = children;
     if (isEnd) {
@@ -62,10 +92,20 @@ public class Trie implements Serializable {
     return names;
   }
 
+  /**
+   * Returns children of current Trie node.
+   *
+   * @return map from string to Trie object
+   */
   public Map<String, Trie> getChildren() {
     return this.children;
   }
 
+  /**
+   * Returns whether current node is the end of the name.
+   *
+   * @return boolean
+   */
   public boolean getIsEnd() {
     return this.isEnd;
   }
