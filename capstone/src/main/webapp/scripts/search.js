e1d226cfbd3649bf9835f27720a0b8661d765c9a
Lucy Qu <lucy_qu@brown.edu>
2020-07-22 17:58:28 -0400

    start frontend

---
+++ capstone/src/main/webapp/scripts/search.js
@@ -0,0 +1,8 @@
+function autocomplete() {
+  const searchInput = document.getElementById("name-search");
+  searchInput.addEventListener("keyup", function() {});
+}
+
+function appendSuggestions() {}
+
+autocomplete();
