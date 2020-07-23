c74a2947aa60bc6796cff45e9058a4457540ca2c
Himani Yadav <himaniyadav@google.com>
2020-07-23 19:45:30 +0000

    Move definitions inside of tests

--- capstone/src/test/java/com/google/sps/objects/TFIDFStringHelperTest.java
+++ capstone/src/test/java/com/google/sps/objects/TFIDFStringHelperTest.java
@@ -18,43 +18,10 @@ import com.google.sps.Objects.TFIDFStringHelper;
  */
  @RunWith(JUnit4.class)
 public class TFIDFStringHelperTest {
-  private final String testString = "Don't you dare!!";
-  private final String expectedString = "don't you dare";
-  private final String testStringNumber = "D.on't you 2 dare!,!";
-  private final String expectedStringNumber = "don't you 2 dare";
-
-  private final String testNgrams = "how about them how";
-  private LinkedHashMap<String, Integer> expectedNgramsMap;
-  private final String testNgrams2 = "No not no Not";
-  private LinkedHashMap<String, Integer> expectedNgramsMap2;
-  private final String testNgrams3 = "how about";
-  private LinkedHashMap<String, Integer> expectedNgramsMap3;
 
   @Before
   public void setUp() {
-    expectedNgramsMap = new LinkedHashMap<String, Integer>();
-    expectedNgramsMap.put("how", 2);
-    expectedNgramsMap.put("about", 1);
-    expectedNgramsMap.put("them", 1);
-    expectedNgramsMap.put("how about", 1);
-    expectedNgramsMap.put("about them", 1);
-    expectedNgramsMap.put("them how", 1);
-    expectedNgramsMap.put("how about them", 1);
-    expectedNgramsMap.put("about them how", 1);
-
-
-    expectedNgramsMap2 = new LinkedHashMap<String, Integer>();
-    expectedNgramsMap2.put("no", 2);
-    expectedNgramsMap2.put("not", 2);
-    expectedNgramsMap2.put("no not", 2);
-    expectedNgramsMap2.put("not no", 1);
-    expectedNgramsMap2.put("no not no", 1);
-    expectedNgramsMap2.put("not no not", 1);
-
-    expectedNgramsMap3 = new LinkedHashMap<String, Integer>();
-    expectedNgramsMap3.put("how", 1);
-    expectedNgramsMap3.put("about", 1);
-    expectedNgramsMap3.put("how about", 1);
+    // set up work
   }
 
   @After
@@ -64,11 +31,17 @@ public class TFIDFStringHelperTest {
 
   @Test
   public void sanitizeString() {
+    String testString = "Don't you dare!!";
+    String expectedString = "don't you dare";
+
     assertEquals(TFIDFStringHelper.sanitize(testString), expectedString);
   }
 
   @Test
   public void sanitize_StringWithNumbers() {
+    String testStringNumber = "D.on't you 2 dare!,!";
+    String expectedStringNumber = "don't you 2 dare";
+    
     assertEquals(TFIDFStringHelper.sanitize(testStringNumber), expectedStringNumber);
   }
 
@@ -79,16 +52,45 @@ public class TFIDFStringHelperTest {
 
   @Test
   public void ngramTokenizer() {
+    String testNgrams = "how about them how";
+
+    LinkedHashMap<String, Integer> expectedNgramsMap = new LinkedHashMap<String, Integer>();
+    expectedNgramsMap.put("how", 2);
+    expectedNgramsMap.put("about", 1);
+    expectedNgramsMap.put("them", 1);
+    expectedNgramsMap.put("how about", 1);
+    expectedNgramsMap.put("about them", 1);
+    expectedNgramsMap.put("them how", 1);
+    expectedNgramsMap.put("how about them", 1);
+    expectedNgramsMap.put("about them how", 1);
+
     assertEquals(TFIDFStringHelper.ngramTokenizer(testNgrams), expectedNgramsMap);
   }
 
   @Test
   public void ngramTokenizer_notSanitized() {
+    String testNgrams2 = "No not no Not";
+
+    LinkedHashMap<String, Integer> expectedNgramsMap2 = new LinkedHashMap<String, Integer>();
+    expectedNgramsMap2.put("no", 2);
+    expectedNgramsMap2.put("not", 2);
+    expectedNgramsMap2.put("no not", 2);
+    expectedNgramsMap2.put("not no", 1);
+    expectedNgramsMap2.put("no not no", 1);
+    expectedNgramsMap2.put("not no not", 1);
+
     assertEquals(TFIDFStringHelper.ngramTokenizer(testNgrams2), expectedNgramsMap2);
   }
 
   @Test
   public void ngramTokenizer_twoWords() {
+    String testNgrams3 = "how about";
+
+    LinkedHashMap<String, Integer> expectedNgramsMap3 = new LinkedHashMap<String, Integer>();
+    expectedNgramsMap3.put("how", 1);
+    expectedNgramsMap3.put("about", 1);
+    expectedNgramsMap3.put("how about", 1);
+
     assertEquals(TFIDFStringHelper.ngramTokenizer(testNgrams3), expectedNgramsMap3);
   }
 
