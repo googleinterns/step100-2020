d8c7353639d194e5e71a8d09e14ecc3bb06f313a
Lucy Qu <lucy_qu@brown.edu>
2020-07-24 12:30:52 -0400

    close files in save state

--- capstone/src/main/java/com/google/sps/search/servlets/NameDataServlet.java
+++ capstone/src/main/java/com/google/sps/search/servlets/NameDataServlet.java
@@ -24,45 +24,6 @@ public class NameDataServlet extends AuthenticatedServlet {
   private SearchPredictor searchPredictor;
   private final String TRIE_FILE = "../../data/trie";
 
-  //  @Override
-  //  public void init(ServletConfig config) throws ServletException {
-  //    super.init(config);
-  //    FileInputStream fileInput = null;
-  //    ObjectInputStream objectInput = null;
-  //    try {
-  //      fileInput = new FileInputStream(new File(TRIE_FILE));
-  //      objectInput = new ObjectInputStream(fileInput);
-  //    } catch (FileNotFoundException e) {
-  //      System.err.println("File does not exist");
-  //    } catch (IOException e) {
-  //      System.err.println("Cannot read from file");
-  //    } finally {
-  //      if (fileInput != null) {
-  //        try {
-  //          fileInput.close();
-  //        } catch (IOException e) {
-  //          System.err.println("Cannot close file");
-  //        }
-  //      }
-  //      if (objectInput != null) {
-  //        try {
-  //          objectInput.close();
-  //        } catch (IOException e) {
-  //          System.err.println("Cannot close file");
-  //        }
-  //      }
-  //    }
-  //
-  //    try {
-  //      searchPredictor = (SearchPredictor) objectInput.readObject();
-  //      return;
-  //    } catch (ClassNotFoundException | IOException e) {
-  //      System.err.println("Class not found");
-  //    }
-  //
-  //    searchPredictor = new SearchPredictor();
-  //  }
-
   @Override
   public void init(ServletConfig config) throws ServletException {
     super.init(config);
@@ -100,19 +61,29 @@ public class NameDataServlet extends AuthenticatedServlet {
   }
 
   public void saveState() {
-    FileOutputStream fileOutputStream;
+    FileOutputStream fileOutputStream = null;
+    ObjectOutputStream objectOutputStream = null;
     try {
       fileOutputStream = new FileOutputStream(new File(TRIE_FILE));
-      ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+      objectOutputStream = new ObjectOutputStream(fileOutputStream);
       objectOutputStream.writeObject(searchPredictor);
 
-      objectOutputStream.close();
-      fileOutputStream.close();
       System.out.println("successfully written to file");
     } catch (FileNotFoundException e1) {
       System.err.println("File does not exist");
     } catch (IOException e) {
       System.err.println("Cannot write to file");
+    }
+
+    try {
+      if (objectOutputStream != null) {
+        objectOutputStream.close();
+      }
+      if (fileOutputStream != null) {
+        fileOutputStream.close();
+      }
+    } catch (IOException e) {
+      System.err.println("Cannot close file");
     }
   }
 
