c2afe34eb14858d8f413bc33dffdcf41285d2e4a
anikabagga <anikabagga@gmail.com>
2020-07-24 07:35:50 +0000

    pr changes

--- capstone/src/main/webapp/scripts/auth.js
+++ capstone/src/main/webapp/scripts/auth.js
@@ -88,7 +88,7 @@ function createNewUser() {
     params.append('interests', interests);
 
     var geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { 'address': address}, function(results, status) {
+    geocoder.geocode({'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         var latitude = results[0].geometry.location.lat();
         var longitude = results[0].geometry.location.lng();
