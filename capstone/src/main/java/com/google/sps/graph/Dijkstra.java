96b562d6bace1546acfef04a09d9c201fac97874
Lucy Qu <quxiaoting2@gmail.com>
2020-08-04 19:16:15 +0000

    remove print lines

--- capstone/src/main/java/com/google/sps/graph/Dijkstra.java
+++ capstone/src/main/java/com/google/sps/graph/Dijkstra.java
@@ -8,7 +8,6 @@ import java.util.Set;
 public class Dijkstra<V extends Vertex<E>, E extends Edge<V>> {
 
   public Vertex<E> runDijkstra(V src, V dest) {
-    System.out.println("running dijkstra");
     // Used to keep track of vertices closest to the source vertex
     PriorityQueue<V> minHeap = new PriorityQueue<V>(new VertexComparator<E>());
     src.setDistance(0);
@@ -17,43 +16,33 @@ public class Dijkstra<V extends Vertex<E>, E extends Edge<V>> {
     while (!minHeap.isEmpty()) {
       // Pop off vertex with smallest distance from src
       V current = minHeap.peek();
-      visitedSet.add(minHeap.poll());
-      System.out.println("visited set " + visitedSet);
-      System.out.println("current " + current.getId() + " " + current + " " + "dest " + dest.getId());
+      visitedSet.add(minHeap.poll()););
       if (current.equals(dest)) {
-        System.out.println("current equals dest----------------------");
         return current;
       }
       List<E> outgoingEdges = current.getOutgoingEdges();
-      System.out.println("outgoing edges dijkstra " + outgoingEdges);
       if (outgoingEdges == null) {
         return null;
       }
       for (E e : outgoingEdges) {
         // Get the vertex on the other side of the edge
         V adjacent = e.getDestVertex();
-        System.out.println("adjacent " + adjacent.getId() + " " + adjacent);
         // Update the shortest path to adjacent vertex
         double newDistanceToAdjacent = current.getDistance() + e.getEdgeWeight();
-        System.out.println("new calculated distance " + newDistanceToAdjacent + "old distance" + adjacent.getDistance());
         if (newDistanceToAdjacent < adjacent.getDistance()
             || (!minHeap.contains(adjacent) && !visitedSet.contains(adjacent))) {
-          System.out.println("resetting distance");
           adjacent.setDistance(newDistanceToAdjacent);
           // remember path to this vertex
           adjacent.setPrev(e);
           // Trigger reheapify since vertex has been mutated
           if (minHeap.contains(adjacent)) {
-            System.out.println("reheapify");
             minHeap.add(minHeap.remove());
           } else {
-            System.out.println("adding adjacentn to minheap");
             minHeap.add(adjacent);
           }
         }
       }
     }
-    System.out.println("returning dest ---------------------" + dest.getDistance());
     return dest;
   }
 }
