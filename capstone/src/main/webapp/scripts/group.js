a6ade5160f7e42bab10f9be4efb384c6ba5b0076
anikabagga <anikabagga@gmail.com>
2020-08-06 17:53:37 +0000

    spacing

--- capstone/src/main/webapp/scripts/group.js
+++ capstone/src/main/webapp/scripts/group.js
@@ -59,7 +59,7 @@ function getGroupId() {
 
 function findClosestGroupLocations() {
   fetch(`/create-quadtree?groupId=${groupId}`, { method: "POST" }).then(() => {
-		loadClosestGroupLocations();
+    loadClosestGroupLocations();
   });
 }
 
